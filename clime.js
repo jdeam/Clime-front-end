088190cb5246274212f6f50f9e350c86d5c7fc5d
Jordan Deam <jordandeam@Jordans-MacBook-Pro.local>
2018-02-07 16:17:58 -0800

    Added styling with Bulma

--- clime.js
+++ clime.js
@@ -1,13 +1,113 @@
-//Index: https://api.weather.gov/gridpoints/SEW/150,72/forecast/hourly
-//47.82, -121.556
+//Index: 47.82, -121.556
+let ctx = document.getElementById("myChart").getContext("2d");
+
+function renderChart(ctx, forecast) {
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: forecast.time,
+      datasets: [{
+        label: 'Temp',
+        yAxisID: 'Temp',
+        data: forecast.temp,
+        fill: false,
+        borderColor: "#c45850",
+        backgroundColor: 'rgba(196, 88, 80, 0.3)'
+      }, {
+        label: 'Precip',
+        yAxisID: 'Precip',
+        data: forecast.precip,
+        fill: true,
+        borderColor: "#3e95cd",
+        backgroundColor: 'rgba(62, 149, 205, 0.3)'
+      }]
+    },
+    options: {
+      elements: {
+        point: {
+          radius: 1.5
+        }
+      },
+      legend: {
+        display: false
+      },
+      tooltips : {
+        intersect: false,
+        callbacks: {
+          title: function(t, d) {
+            return moment(t[0].xLabel).format('LT');
+          },
+          label: function(t, d) {
+            if (t.datasetIndex === 0) {
+              return `${t.yLabel}°F`;
+            } else if (t.datasetIndex === 1) {
+              return `${t.yLabel}%`;
+            }
+          }
+        }
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: false
+      },
+      scales: {
+        xAxes: [{
+          type: 'time',
+          time: {
+            day: 'day',
+            displayFormats: {
+              hour: 'ddd, MMM D',
+              day: 'ddd, MMM D'
+            }
+          },
+          gridLines: {
+            offsetGridLines: false,
+            lineWidth: 1
+          },
+          ticks: {
+            source: 'auto',
+            callback: function(value, index, values) {
+              return value;
+            }
+          }
+        }],
+        yAxes: [{
+          id: 'Temp',
+          type: 'linear',
+          position: 'left',
+          ticks: {
+            max: forecast.maxTemp,
+            min: forecast.minTemp,
+            stepSize: (forecast.maxTemp-forecast.minTemp)/5,
+            callback: function(value, index, values) {
+              return `${parseFloat(value).toFixed(0)}°F`
+            }
+          }
+        }, {
+          id: 'Precip',
+          type: 'linear',
+          position: 'right',
+          ticks: {
+            max: 100,
+            min: 0,
+            callback: function(value, index, values) {
+              return `${value}%`
+            }
+          }
+        }]
+      }
+    }
+  });
+}
 
 let forecast;
-let hourly = [];
-axios.get('http://localhost:3000/forecast').then(result => {
-  forecast = result.data.data.hourly.data;
-  forecast.forEach(hour => {
-    hourly.push(hour.temperature);
-  })
-});
+let saved;
+axios.get('http://localhost:3000/forecast')
+  .then(result => {
+    forecast = result.data.forecast;
+    renderChart(ctx, result.data.forecast);
+  });
 
-let ctx = document.getElementById("myChart").getContext("2d");
+navigator.geolocation.getCurrentPosition(function(position) {
+  console.log(position);
+});
