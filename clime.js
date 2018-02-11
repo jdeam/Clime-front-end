0d1a23ec638b6c5b976e48a18391bf8745bf3999
Jordan Deam <jordandeam@Jordans-MacBook-Pro.local>
2018-02-10 17:51:46 -0800

    Started maps integration

--- clime.js
+++ clime.js
@@ -115,6 +115,14 @@ axios.get('http://localhost:3000/crags')
     });
   });
 
+// var map;
+// function initMap() {
+// map = new google.maps.Map(document.getElementById('map'), {
+//   zoom: 2,
+//   center: new google.maps.LatLng(2.8,-187.3),
+//   mapTypeId: 'terrain'
+// });
+
 axios.get('http://localhost:3000/crags/1').then(result => {
   console.log(result.data);
 });
